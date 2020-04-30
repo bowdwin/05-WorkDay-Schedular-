ce19a81c97882eb6f75b76599a9d4afdc6dad227
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-04-30 09:29:19 -0400

    saving to one element in data storage

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ $(".save-button").on("click", function () {
 
     // stores input text into inputtext
     // var storeData = []
-    var inputText = $(".input-text-8").val();
+
+    //this is working kinda stores in one variable
+    var inputText = $(".input-text-" + this.id).val();
+    console.log(inputText + " input text is");
 
     console.log(this.id + " this item ID");
     console.log(inputText + " this is input text");
@@ -18,7 +21,8 @@ $(".save-button").on("click", function () {
     storeData.push({ textValue: inputText, id: this.id });
     var storeDataLocalStorage = JSON.stringify(storeData);
 
-    localStorage.setItem("item", storeDataLocalStorage);
+    localStorage.setItem("data", storeDataLocalStorage);
+
 
     // scores.push({ name: names.value, score: score });
     // var scoresStringed = JSON.stringify(scores);
@@ -28,7 +32,7 @@ $(".save-button").on("click", function () {
 
     //need to call a function to check if something exist and replace it or push it 
     //should be another function
-    console.log(storeData);
+
 
 
 });
