7f1c6828c0da797d66ee9695190e654fe131ad2a
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-05-01 15:48:30 -0400

    got everything working using different storage keys instead of 1

--- script.js
+++ script.js
@@ -1,55 +1,39 @@
-
-
 var storeData = [];
-
 initialLocalStoragePlace();
 
-
+$("#currentDay").text(moment().format('LL'));
 $(".save-button").on("click", function (event, state) {
-
     var inputText = $(".input-text-" + this.id).val();
     var thisIstheId = this.id;
-    console.log(inputText + " this is input text");
-    console.log(thisIstheId + " this is input text");
-
-
     checkifSavedInArray(inputText, thisIstheId);
-
 });
-// });
-
-
 function checkifSavedInArray(inputText, thisIstheId) {
-
     newArray = [];
-    console.log(thisIstheId + " this item ID");
-    console.log(inputText + " this is input text");
     newArray.push({ textValue: inputText, id: thisIstheId });
     var storeDataLocalStorage = JSON.stringify(newArray);
     localStorage.setItem(thisIstheId, storeDataLocalStorage);
-
-
 }
-
 function initialLocalStoragePlace() {
-
     var totalCal = 18;
-
+    var currentTime = moment().format('H');
+    console.log(currentTime);
     for (var i = 8; i < totalCal; i++) {
-
         var dataOfStorage = JSON.parse(localStorage.getItem(i));
-        console.log(dataOfStorage);
         if (dataOfStorage != null) {
-            console.log(dataOfStorage + " data of strage ");
             var textValueVar = dataOfStorage[0].textValue;
-            console.log(textValueVar + " text value var");
             var dataIdVar = dataOfStorage[0].id;
             let input = $(".input-text-" + dataIdVar);
             input.val(input.val() + textValueVar);
-
         }
-
-
+        if (currentTime > i) {
+            $(".input-text-" + i).addClass("past");
+        }
+        else if (currentTime < i) {
+            $(".input-text-" + i).addClass("future");
+        }
+        else {
+            $(".input-text-" + i).addClass("present");
+        }
     }
-
 }
+
